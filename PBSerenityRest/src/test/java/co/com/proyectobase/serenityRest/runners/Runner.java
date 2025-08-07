0d05d1e0552160caf510db4fe636550a40a2ecfd
JuanAlvarezB <jcabdim12@hotmail.com>
2025-08-07 12:27:55 -0500

    Cambiar a etiqueta general en el runner

--- PBSerenityRest/src/test/java/co/com/proyectobase/serenityRest/runners/Runner.java
+++ PBSerenityRest/src/test/java/co/com/proyectobase/serenityRest/runners/Runner.java
@@ -9,7 +9,7 @@ import org.junit.runner.RunWith;
 @CucumberOptions(
         features = "src/test/resources/features",
         glue = "co.com.proyectobase.serenityRest.stepdefinitions",
-        tags = "@TipoPut",
+        tags = "@ConsumirSerenityRest",
         snippets = CucumberOptions.SnippetType.CAMELCASE
 )
 public class Runner {
